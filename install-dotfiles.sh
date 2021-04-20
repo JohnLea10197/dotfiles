190fdb5af3d77df6bf5e208027754a7589499592
JohnLea10197 <johnlea10197@gmail.com>
2021-04-19 20:18:36 -0400

    added polybar configs and installer

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -11,20 +11,18 @@ cd ~/.config
 #remove existing symlinks, you should only ever run this script once but fail-safes or whatever. Although if I add more dotfiles in the future running it again will be necessary so future-proofing
 rm -rf mpd & echo "deleted existing symlink for mpd config files"
 rm -rf ncmpcpp & echo "deleted existing symlink for ncmpcpp config files"
-rm -rf nitrogen & echo "deleted existing symlink for nitrogen config files"
 rm -rf nvim & echo "deleted existing symlink for neovim config files"
-rm -rf pulse & echo "deleted existing symlink for pulseaudio config files"
+rm -rf polybar & echo "deleted existing symlink for polybar"
 rm -rf zsh & echo -e "deleted existing symlink for zsh config files \n"
 
 #add new symlinks
 ln -s $dothome/mpd mpd & echo "successfully created symlink for mpd"
 ln -s $dothome/ncmpcpp ncmpcpp & echo "successfully created symlink for ncmpcpp"
-ln -s $dothome/nitrogen nitrogen & echo "successfully created symlink for nitrogen"
 ln -s $dothome/nvim nvim & echo "successfully created symlink for neovim"
-ln -s $dothome/pulse pulse & echo -e "successfully created symlink for pulseaudio"
+ln -s $dothome/polybar polybar & echo "successfully created symlink for polybar"
 ln -s $dothome/zsh zsh & echo -e "successfully created symlink for zsh \n"
 
-#add new symlinks that aren't in the usual .config directory (although I wish they were)
+#add new symlinks that aren't in the usual .config directory
 cd ~/
 
 cd $dothome
@@ -36,8 +34,8 @@ pwd
 cd $dothome
 rm -rf mpd/mpd
 rm -rf ncmpcpp/ncmpcpp
-rm -rf nitrogen/nitrogen
 rm -rf nvim/nvim
-rm -rf pulse/pulse
+rm -rf polybar/polybar
+rm -rf zsh/zsh
 
 cd $dothome
---
+++ polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Terminate existing polybar instances
+killall -q polybar
+
+#Wait until polybar shuts down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+#Launch polybar with default config
+polybar status &
+
+#Tell the terminal that the bar was launched
+echo "Polybar named "status" launched..."
