a8da1a5c3e630bc6d6b9b78a61ea0af4a4cf8481
JohnLea10197 <johnlea10197@protonmail.com>
2021-04-26 00:27:17 -0400

    making bspwm and sxhkd comfy; configured polybar to look and play nicely

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,6 +8,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 #Launch polybar with default config
 polybar status &
+polybar status-mon2 &
 
 #Tell the terminal that the bar was launched
 echo "Polybar named "status" launched..."
--- polybar/mpdscroll.sh
+++ polybar/mpdscroll.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-if ! mpc >/dev/null 2>&1; then
-  echo Server offline
-  exit 1
-elif mpc status | grep -q playing; then
- ( mpc current | zscroll -p "" -l 20 -d 0.3 -n 1 ) &
-else
-  echo Not playing
-fi
-# Block until an event is emitted
-mpc idle >/dev/null
+#if ! mpc >/dev/null 2>&1; then
+#  echo Server offline
+#  exit 1
+#elif mpc status | grep -q playing; then
+# ( mpc current | zscroll -b " " -p "" -l 20 -d 0.3 -n 1 ) &
+#else
+#  echo Not playing
+#fi
+## Block until an event is emitted
+#mpc idle >/dev/null
+
+zscroll --before-text "♪ x" --delay 0.3 -l 20 \
+		--match-command "mpc status" \
+		--match-text "playing" "--before-text ' '" \
+		--match-text "paused" "--before-text ' ' --scroll 0" \
+		--update-check true "mpc current" &
+
+wait
